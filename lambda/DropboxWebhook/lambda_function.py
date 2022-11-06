f917d6a6526fd47ae071adc31f9f574e5ede4d05
ConstantinB9 <constantin.braess@gmail.com>
2022-11-06 21:21:18 +0000

    get dropbox credentials from dynamodb

--- lambda/DropboxWebhook/lambda_function.py
+++ lambda/DropboxWebhook/lambda_function.py
@@ -10,13 +10,12 @@ requests_log = logging.getLogger("requests.packages.urllib3")
 requests_log.setLevel(logging.DEBUG)
 requests_log.propagate = True
 
-def process_account(account):
-    pass
-
 
 def lambda_handler(event, context):
     print(event)
     print(context)
+    
+    dynamodb_client = boto3.client("dynamodb")
 
     # handle challenge
     event = event if event is not None else {}
@@ -33,7 +32,21 @@ def lambda_handler(event, context):
 
     signature = event.get("headers", {}).get("X-Dropbox-Signature", "")
     payload = event.get("body", {})
-    app_secret = "" # todo: fetch
+    
+    try:
+        app_secret = dynamodb_client.get_item(
+            TableName="whlsckr_app_credentials",
+            Key={
+                'AppName': {"S": "Dropbox"}
+            }
+        )["Item"]["Secret"]["S"]
+    except KeyError:
+        logging.error("App Secret for 'Dropbox' in table 'whlsckr_app_credentials' not found")
+        return {
+            "statusCode": 500,
+            "details": "Internal Server Error"
+        }
+
     try:
         valid = hmac.compare_digest(signature, hmac.new(app_secret.encode(), json.dumps(payload, sort_keys=True).encode(), sha256).hexdigest())
     except TypeError:
@@ -46,21 +59,13 @@ def lambda_handler(event, context):
     print("Validated")
     
     lmd_client = boto3.client("lambda")
-    response = lmd_client.invoke(
-        FunctionName="arn:aws:lambda:eu-central-1:574639460976:function:ProcessUserUpdate",
-        InvocationType="RequestResponse",
-        Payload=json.dumps({})
-    )
-    
-    print("Response: ")
-    print(response)
-    print(json.load(response['Payload']))
-    
-
-    import threading
 
     for account in payload.get("list_folder", {}).get("accounts", []):
-        threading.Thread(target=process_account, args=(account,)).start()
+        lmd_client.invoke(
+        FunctionName="arn:aws:lambda:eu-central-1:574639460976:function:ProcessUserUpdate",
+        InvocationType="Event",
+        Payload=json.dumps({'account': account})
+    )
 
     return {
         "statusCode": 200,
--- terraform/main.tf
+++ terraform/main.tf
@@ -306,3 +306,58 @@ resource "aws_lambda_permission" "api_gw" {
 
   source_arn = "${aws_apigatewayv2_api.whlsckr_api.execution_arn}/*/*"
 }
+
+
+########## DYNAMO DB ##########
+
+resource "aws_dynamodb_table" "user_data" {
+  name           = "whlsckr_user_data"
+  billing_mode   = "PROVISIONED"
+  read_capacity  = 1
+  write_capacity = 1
+  hash_key       = "UserId"
+
+
+  attribute {
+    name = "UserId"
+    type = "N"
+  }
+
+  attribute {
+    name = "DropboxId"
+    type = "N"
+  }
+
+  global_secondary_index {
+    name               = "DropboxIndex"
+    hash_key           = "DropboxId"
+    write_capacity     = 1
+    read_capacity      = 1
+    projection_type    = "INCLUDE"
+    non_key_attributes = ["UserId"]
+  }
+
+  tags = {
+    Name        = "whlsckr-user-data"
+    Environment = "production"
+  }
+}
+
+resource "aws_dynamodb_table" "app-credentials" {
+  name           = "whlsckr_app_credentials"
+  billing_mode   = "PROVISIONED"
+  read_capacity  = 1
+  write_capacity = 1
+  hash_key       = "AppName"
+
+
+  attribute {
+    name = "AppName"
+    type = "S"
+  }
+
+  tags = {
+    Name        = "whlsckr-app-data"
+    Environment = "production"
+  }
+}
